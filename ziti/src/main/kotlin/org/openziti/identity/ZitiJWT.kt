194a8119ca4688d99ee2506c4466887833be87bf
Eugene K <eugene.kobyakov@netfoundry.io>
2020-05-26 10:32:12 -0400

    org.openziti umbrella rename

---
+++ ziti/src/main/kotlin/org/openziti/identity/ZitiJWT.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2018-2020 NetFoundry, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openziti.identity
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.JwsHeader
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SigningKeyResolver
+import java.net.URI
+import java.net.URL
+import java.security.Key
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
+
+class ZitiJWT(cl: Claims, val serverKey: Key) {
+    private val claims = cl
+
+    val controller: URI by lazy {
+        URI.create(claims.get("iss", String::class.java))
+    }
+
+    val token: String by lazy {
+        claims.get("jti", String::class.java)
+    }
+    val method: String by lazy {
+        claims.get("em", String::class.java)
+    }
+
+    val name: String
+        get() = claims.get("sub", String::class.java)
+
+    val enrollmentURL: URL
+        get() = controller.resolve("/enroll?method=${method}&token=${token}").toURL()
+
+    companion object {
+        fun fromJWT(jwt: String): ZitiJWT {
+            val jwtParser = Jwts.parser()
+            val tm = JwtTrustManager()
+
+            jwtParser.setSigningKeyResolver(KeyResolver(tm))
+
+            val claims = jwtParser.parse(jwt).body as Claims
+            return ZitiJWT(claims, tm.serverKey)
+        }
+    }
+
+    class JwtTrustManager : X509TrustManager {
+        lateinit var serverKey: Key
+        override fun checkClientTrusted(p0: Array<out X509Certificate>?, p1: String?) = TODO("not needed")
+
+        override fun checkServerTrusted(certs: Array<out X509Certificate>?, authType: String) {
+            serverKey = checkNotNull(certs?.first()?.publicKey)
+        }
+
+        override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+    }
+
+    class KeyResolver(val tm: JwtTrustManager) : SigningKeyResolver {
+
+        override fun resolveSigningKey(header: JwsHeader<*>, claims: Claims): Key? {
+            val ssl = SSLContext.getInstance("TLSv1.2").apply {
+                init(null, arrayOf(tm), SecureRandom())
+            }
+
+            val url = URI.create(claims.get("iss").toString()).toURL()
+            val conn = url.openConnection() as HttpsURLConnection
+            conn.sslSocketFactory = ssl.socketFactory
+
+            try {
+                conn.connect()
+                return tm.serverKey
+            } finally {
+                conn.disconnect()
+            }
+        }
+
+        // this is not used
+        override fun resolveSigningKey(header: JwsHeader<*>, plaintext: String) = TODO()
+    }
+}
---
+++ ziti/src/main/kotlin/org/openziti/util/Certs.kt
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2018-2020 NetFoundry, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openziti.util
+
+import org.bouncycastle.openssl.PEMKeyPair
+import org.bouncycastle.openssl.PEMParser
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
+import org.bouncycastle.operator.ContentSigner
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder
+import org.bouncycastle.util.io.pem.PemReader
+import java.io.OutputStream
+import java.io.Reader
+import java.net.Socket
+import java.net.URI
+import java.nio.charset.Charset
+import java.security.*
+import java.security.cert.CertificateException
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import java.util.*
+import javax.net.ssl.*
+
+internal class KeyTrustManager(val key: Key) : X509TrustManager {
+    override fun checkServerTrusted(certs: Array<out X509Certificate>, authType: String) {
+        check(certs[0].publicKey.equals(key)) {
+            throw CertificateException("untrusted server")
+        }
+    }
+
+    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+    override fun checkClientTrusted(p0: Array<out X509Certificate>, p1: String) = TODO("not used")
+}
+
+/**
+ * [KeyManager] that narrows key store to a single identity
+ */
+internal class AliasKeyManager(val alias: String, delegate: KeyManager) : X509ExtendedKeyManager() {
+    val x509 = delegate as X509KeyManager
+    override fun getClientAliases(keyType: String?, issuers: Array<out Principal>?): Array<String> = arrayOf(alias)
+
+    override fun getServerAliases(keyType: String?, issuers: Array<out Principal>?): Array<String> =
+        TODO("not implemented")
+
+    override fun chooseServerAlias(keyType: String?, issuers: Array<out Principal>?, socket: Socket?) =
+        TODO("not implemented")
+
+    override fun getCertificateChain(alias: String?): Array<X509Certificate> = x509.getCertificateChain(alias)
+
+    override fun getPrivateKey(alias: String?): PrivateKey? = x509.getPrivateKey(alias)
+
+    override
+    fun chooseEngineClientAlias(keyType: Array<out String>?, issuers: Array<out Principal>?, engine: SSLEngine?) =
+        alias
+
+    override
+    fun chooseClientAlias(keyType: Array<out String>?, issuers: Array<out Principal>?, socket: Socket?) = alias
+
+    companion object {
+        fun from(alias: String, kms: Array<KeyManager>): Array<X509KeyManager> =
+            kms.map { AliasKeyManager(alias, it) }.toTypedArray()
+    }
+}
+
+
+internal val cf = CertificateFactory.getInstance("X.509")
+
+fun readCerts(pem: String) = readCerts(pem.reader())
+
+fun readCerts(pemInput: Reader) = PemReader(pemInput).use { reader ->
+    generateSequence { reader.readPemObject() }
+        .filter { it.type == "CERTIFICATE" }
+        .map { cf.generateCertificate(it.content.inputStream()) as X509Certificate }
+        .toList()
+}
+
+fun readKey(input: Reader): PrivateKey {
+    val parser = PEMParser(input)
+    val po = parser.readObject() as PEMKeyPair
+    return JcaPEMKeyConverter().getKeyPair(po).private
+}
+
+internal class PrivateKeySigner(val key: PrivateKey, val sigAlg: String) : ContentSigner {
+    val sig = Signature.getInstance(sigAlg)
+
+    init {
+        sig.initSign(key)
+    }
+
+    override fun getAlgorithmIdentifier() = DefaultSignatureAlgorithmIdentifierFinder().find(sigAlg)
+
+    override fun getOutputStream() = object : OutputStream() {
+        override fun write(b: Int) = sig.update(b.toByte())
+        override fun write(b: ByteArray?) = sig.update(b)
+        override fun write(b: ByteArray?, off: Int, len: Int) = sig.update(b, off, len)
+    }
+
+    override fun getSignature(): ByteArray = sig.sign()
+}
+
+internal fun getCACerts(api: URI, serverKey: Key): Collection<X509Certificate> {
+    val con = api.resolve("/.well-known/est/cacerts").toURL().openConnection() as HttpsURLConnection
+    con.sslSocketFactory = with(SSLContext.getInstance("TLSv1.2")) {
+        init(null, arrayOf(KeyTrustManager(serverKey)), SecureRandom())
+        socketFactory
+    }
+
+    con.doInput = true
+
+    if (con.responseCode == 200) {
+        con.inputStream.use {
+            val b = it.readBytes().toString(Charset.defaultCharset())
+            val bytes = Base64.getMimeDecoder().decode(b)
+            val cf = CertificateFactory.getInstance("X.509")
+
+            return cf.generateCertificates(bytes.inputStream()) as Collection<X509Certificate>
+        }
+    }
+
+    return emptyList()
+}
