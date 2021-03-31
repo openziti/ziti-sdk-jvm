bbcb563ceba50f1ba40de52cf405e7eb6274b17f
Eugene K <eugene.kobyakov@netfoundry.io>
2021-03-31 12:54:45 -0400

    implement ZitiResolverGroup to support connecting by intercept address

--- ziti-netty/src/main/kotlin/org/openziti/netty/ZitiChannel.kt
+++ ziti-netty/src/main/kotlin/org/openziti/netty/ZitiChannel.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020 NetFoundry, Inc.
+ * Copyright (c) 2018-2021 NetFoundry, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -75,7 +75,7 @@ class ZitiChannel(parent: ServerChannel?, val peer: AsynchronousSocketChannel):
     }
 
     override fun doClose() {
-        peer.close()
+        runCatching { peer.close() }
     }
 
     override fun newUnsafe(): AbstractUnsafe = AsyncUnsafe()
