06ba4d2c8257dc71d82b19c27dbc1493581c5cb4
ekoby <7406535+ekoby@users.noreply.github.com>
2021-11-15 17:29:34 -0500

    implement BypassSocket (over async socket) (#273)

--- ziti/src/main/kotlin/org/openziti/net/internal/Sockets.kt
+++ ziti/src/main/kotlin/org/openziti/net/internal/Sockets.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020 NetFoundry, Inc.
+ * Copyright (c) 2018-2021 NetFoundry Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,8 +21,11 @@ import org.openziti.net.ZitiSocketFactory
 import org.openziti.net.nio.AsyncSocketImpl
 import org.openziti.util.Logged
 import org.openziti.util.ZitiLog
+import java.net.InetAddress
+import java.net.InetSocketAddress
 import java.net.Socket
 import java.util.concurrent.atomic.AtomicBoolean
+import javax.net.SocketFactory
 
 internal object Sockets : Logged by ZitiLog() {
 
@@ -42,5 +45,38 @@ internal object Sockets : Logged by ZitiLog() {
     }
 
     fun isInitialized() = initialized.get()
+
+    class BypassSocket: Socket(AsyncSocketImpl())
+
+    class BypassSocketFactory: SocketFactory() {
+
+        override fun createSocket(): Socket = BypassSocket()
+
+        override fun createSocket(host: String?, port: Int): Socket {
+            val s = createSocket()
+            s.connect(InetSocketAddress(host, port))
+            return s
+        }
+
+        override fun createSocket(host: String?, port: Int, localhost: InetAddress?, localport: Int): Socket {
+            val s = createSocket()
+            s.connect(InetSocketAddress(host, port))
+            return s
+        }
+
+        override fun createSocket(addr: InetAddress?, port: Int): Socket {
+            val s = createSocket()
+            s.connect(InetSocketAddress(addr, port))
+            return s
+        }
+
+        override fun createSocket(addr: InetAddress?, port: Int, localhost: InetAddress?, localport: Int): Socket {
+            val s = createSocket()
+            s.connect(InetSocketAddress(addr, port))
+            return s
+        }
+    }
+
+    fun bypassSocketFactory(): SocketFactory = BypassSocketFactory()
 }
 
