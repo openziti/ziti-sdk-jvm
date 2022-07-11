b72610998e204f6bbad902af9a0f233ed6be73f5
Tod Burtchell <tod.burtchell@netfoundry.io>
2022-07-11 15:02:47 -0400

    Removing maven build files/wrappers/etc from spring jpa example

--- samples/ziti-jdbc-spring-jpa/network/startup.sh
+++ samples/ziti-jdbc-spring-jpa/network/startup.sh
@@ -1,3 +1,14 @@
+
+if [ ! -f "simplified-docker-compose.yml" ]; then
+   echo Pulling simplified-docker-compose.yml
+   curl -o simplified-docker-compose.yml "https://raw.githubusercontent.com/openziti/ziti/release-next/quickstart/docker/simplified-docker-compose.yml"
+fi
+
+
+if [ ! -f ".env" ]; then
+   echo "Pulling the docker environment file"
+   curl -o .env https://raw.githubusercontent.com/openziti/ziti/release-next/quickstart/docker/.env
+fi
+
 echo Starting Ziti network
 docker-compose -p zjpa -f simplified-docker-compose.yml -f docker-compose.yml up -d
-
